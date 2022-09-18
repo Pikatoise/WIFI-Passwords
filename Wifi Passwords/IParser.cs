49d7533c09d215866e111d6da6d3493dea2d6ad1
Pikatoise <wierwolf@hotmail.com>
2022-09-18 11:49:59 +0500

    Убрал лишние using'и

--- Wifi Passwords/IParser.cs
+++ Wifi Passwords/IParser.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Wifi_Passwords
+﻿namespace Wifi_Passwords
 {
     public interface IParser
     {
